1843eed28d89deadef69e32edfc806951d3d283e
stephaneworkspace <s.bressani@bluewin.ch>
2019-05-14 14:19:38 +0200

    Début de la migration

---
+++ PartagesWebBlazorFSCore3.Shared/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartagesWebBlazorFSCore3.Shared.Models
+{
+    /// <summary>
+    /// Model User
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+        /// <summary>
+        /// Password Hash
+        /// </summary>
+        public byte[] PasswordHash { get; set; }
+        /// <summary>
+        /// Password Salt
+        /// </summary>
+        public byte[] PasswordSalt { get; set; }
+        /// <summary>
+        /// Date Created
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
