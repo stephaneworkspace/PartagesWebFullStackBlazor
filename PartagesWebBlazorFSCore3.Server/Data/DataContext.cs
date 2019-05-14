6ca3803fa71c46e4d806e8b3b35eadaa40b905f3
stephaneworkspace <s.bressani@bluewin.ch>
2019-05-14 14:47:41 +0200

    migartion, backup

---
+++ PartagesWebBlazorFSCore3.Server/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PartagesWebBlazorFSCore3.Shared.Models;
+using PartagesWebBlazorFSCore3.Shared.Models.Message;
+
+namespace PartagesWebBlazorFSCore3.Server.Data
+{
+    public class DataContext: DbContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        /// <summary>  
+        /// Constructor
+        /// </summary>  
+        /// <param name="options">DbContextOptions</param>
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        /// <summary>  
+        /// Entity User
+        /// </summary> 
+        public DbSet<User> Users { get; set; }
+        /// <summary>  
+        /// Entity Message
+        /// </summary> 
+        public DbSet<Message> Messages { get; set; }
+    }
+}
--- PartagesWebBlazorFSCore3.Server/Startup.cs
+++ PartagesWebBlazorFSCore3.Server/Startup.cs
@@ -1,20 +1,41 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
+using PartagesWebBlazorFSCore3.Server.Data;
 using System.Linq;
 
 namespace PartagesWebBlazorFSCore3.Server
 {
     public class Startup
     {
+        /// <summary>  
+        /// Variable for configuration
+        /// </summary> 
+        public IConfiguration Configuration { get; }
+        /// <summary>  
+        /// Constructor
+        /// </summary> 
+        /// <param name="configuration">Configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().AddNewtonsoftJson();
+            services.AddSwaggerDocument();
+            // services.AddCors();
+            services.AddAutoMapper();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
@@ -39,6 +60,13 @@ namespace PartagesWebBlazorFSCore3.Server
             {
                 endpoints.MapDefaultControllerRoute();
             });
+
+            // cut
+            // app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+            // cut
 
             app.UseBlazor<Client.Startup>();
         }
