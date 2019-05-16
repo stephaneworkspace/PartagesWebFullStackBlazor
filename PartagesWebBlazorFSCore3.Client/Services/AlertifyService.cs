6213ce49a850e588b64af50b81685ee545a0032c
stephaneworkspace <s.bressani@bluewin.ch>
2019-05-16 23:33:17 +0200

    backup

--- PartagesWebBlazorFSCore3.Client/Services/AlertifyService.cs
+++ PartagesWebBlazorFSCore3.Client/Services/AlertifyService.cs
@@ -10,14 +10,37 @@ namespace PartagesWebBlazorFSCore3.Client.Services
     {
         private readonly IJSRuntime _jsRuntime;
 
+        public enum Type
+        {
+            Success,
+            Error,
+            Warning,
+            Default
+        }
+
         public AlertifyService(IJSRuntime jsRuntime)
         {
             _jsRuntime = jsRuntime;
         }
 
-        public async void Open(string message)
+        public async void Open(string message, Type type)
         {
-            var syncJsRunTime = await this._jsRuntime.InvokeAsync<bool>("Alertify", message, "success", 5);
+            switch (type)
+            {
+                case Type.Success:
+                    await this._jsRuntime.InvokeAsync<bool>("Alertify", message, "success", 5);
+                    break;
+                case Type.Error:
+                    await this._jsRuntime.InvokeAsync<bool>("Alertify", message, "error", 5);
+                    break;
+                case Type.Warning:
+                    await this._jsRuntime.InvokeAsync<bool>("Alertify", message, "warning", 5);
+                    break;
+                default:
+                    await this._jsRuntime.InvokeAsync<bool>("Alertify", message, 5);
+                    break;
+            }
+            var syncJsRunTime = await this._jsRuntime.InvokeAsync<bool>("Alertify", message, type, 5);
         }
     }
 }
