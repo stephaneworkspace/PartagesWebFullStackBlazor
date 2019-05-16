14182081c4494d934dd75548df8f22c375e11061
stephaneworkspace <s.bressani@bluewin.ch>
2019-05-16 14:43:16 +0200

    backup

--- PartagesWebBlazorFSCore3.Shared/Dtos/Input/Auth/UserForRegisterInputDto.cs
+++ PartagesWebBlazorFSCore3.Shared/Dtos/Input/Auth/UserForRegisterInputDto.cs
@@ -18,13 +18,15 @@ namespace PartagesWebBlazorFSCore3.Shared.Dtos.Input.Auth
         /// <summary>
         /// Username
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "Le champ « {0} » est obligatoire.")]
+        [Display(Name = "Nom d'utilisateur")]
         [StringLength(30, MinimumLength = 2, ErrorMessage = "Vous devez spécifier un nom d'utilisateur entre 2 et 30 caractères")]
         public string Username { get; set; }
         /// <summary>
         /// Password
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "Le champ « {0} » est obligatoire.")]
+        [Display(Name = "Mot de passe")]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "Vous devez spécifier un mot de passe entre 4 et 8 caractères")]
         public string Password { get; set; }
     }
